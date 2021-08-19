dd68d2a669a393e01bfb290dc0ea9dd9f21c849c
VASILISGEORGATOS <georgatos2008@hotmail.com>
2021-08-19 16:45:19 +0300

    Work

---
+++ WORK/src/containers/_nav.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    _name: 'CSidebarNav',
+    _children: [
+      {
+        _name: 'CSidebarNavItem',
+        name: 'Dashboard',
+        to: '/dashboard',
+        icon: 'cil-speedometer',
+        badge: {
+          color: 'primary',
+          text: 'NEW'
+        }
+      },
+     
+  
+    ]
+  }
+]
